8e3971078bf915e0384220507f96568d1e7167f5
Tau <tauprestat@gmail.com>
2023-11-21 10:56:31 +0100

    Heste og så'n

---
+++ src/Hest.java
@@ -0,0 +1,11 @@
+public class Hest {
+
+    String farve;
+
+    int størrelse;
+
+    public Hest(String farve, int størrelse) {
+        this.farve = farve;
+        this.størrelse = størrelse;
+    }
+}
