6da457f81332364c37ea5cc5db01f6961b467bef
knogleknuser <knogleknuser@hotmail.com>
2023-11-21 11:21:10 +0100

    Was naughty in main

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,72 @@ public class Main {
         System.out.println(bilBil.toString());
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //I am being very naughty
+        //My bullshit console thingy
+        System.out.println();
+        System.out.println();
+        System.out.println("-----Jenny's amazing car!-----");
+        BilBil jennyBil = new BilBil( "Jenny's Øse", 500, true );
+
+        printObject( jennyBil );
+
+        System.out.println();
+        System.out.println("-----Tau's amazing horse!-----");
+        Hest tauHest = new Hest( "Brun", 100 );
+
+        printObject( tauHest );
+
+        System.out.println();
+        System.out.println("-----Steen's amazing furniture!-----");
+
+        Furniture steenFurniture = new Furniture();
+
+        printObject( steenFurniture );
+
+
     } //main
+
+    public static void printObject(Object o) {
+        System.out.println( o.toString() );
+    }
+
 } //class
