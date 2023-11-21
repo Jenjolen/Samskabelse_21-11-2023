8ec13a5437af6cc25c5cbb73c811b39733164117
Jenjolen <cph-jn466@cphbusiness.dk>
2023-11-21 11:08:50 +0100

    laver BilBil i main - skal lave toString i BilBil

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
+    BilBil bilBil = new BilBil("halløjsa", 22, true);
 
+        System.out.println(bilBil.toString());
 
 
     }
