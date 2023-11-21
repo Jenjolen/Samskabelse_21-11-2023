aa29a3d4dce4d779e198a5d7059703117f3d1a35
knogleknuser <knogleknuser@hotmail.com>
2023-11-21 10:48:52 +0100

    Furniture Values

--- src/Furniture.java
+++ src/Furniture.java
@@ -1,2 +1,13 @@
 public class Furniture {
+    int amountOfLegs;
+    String material;
+    boolean isUsable;
+    String brand;
+    String type;
+    String model;
+    String nickname;
+
+
+
+
 }
