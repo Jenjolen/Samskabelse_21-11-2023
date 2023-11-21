2e00e0d0a1b8459d2e53504a753ef22a9a4b7af7
Jenjolen <cph-jn466@cphbusiness.dk>
2023-11-21 11:21:35 +0100

    laver Taxa 4 constructor

--- src/Taxa.java
+++ src/Taxa.java
@@ -6,5 +6,9 @@ public class Taxa {
 
     int antalKilometer;
 
-
+    public Taxa(String taxaselskab, int kilometerPris, int antalKilometer) {
+        this.taxaselskab = taxaselskab;
+        this.kilometerPris = kilometerPris;
+        this.antalKilometer = antalKilometer;
+    }
 }
